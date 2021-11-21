2e1a059c664f4b6c084780a1e9e60df01e5bc82d
yunior <vlado@bodyworld.sk>
2021-11-21 09:19:23 +0100

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=rss_reader',
+    'username' => 'root',
+    'password' => 'testik',
+    'charset' => 'utf8mb4',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
